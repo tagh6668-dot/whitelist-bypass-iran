19ec69f8366757f6e0231de415ca8c84858b4303
kulikov <19497634+kulikov0@users.noreply.github.com>
2026-05-14 23:42:18 +0200

    unbump android version

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 }
 
 val versionMajor = 0
-val versionMinor = 2
-val versionPatch = 1
+val versionMinor = 1
+val versionPatch = 0
 val versionBuild = System.getenv("BUILD_NUMBER")?.toIntOrNull() ?: 0
 
 android {
